b2436777c84d6eb211a79d940a21c696a636584c
sardapv <sardapranav1@gmail.com>
2023-06-30 12:12:03 +0530

    under construction message

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,5 @@
-<div class="">Contact</div>
+<div
+	class="grid place-content-center h-full text-4xl sm:text-7xl font-medium font-gilroy p-4 text-center"
+>
+	Crafting 🔨! Will be live soon 🤩
+</div>
