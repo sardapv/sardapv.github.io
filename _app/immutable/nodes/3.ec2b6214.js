9e30d2772eea776d0a7e5a4f1daa5805ad90efbb
sardapv <sardapranav1@gmail.com>
2023-07-01 13:44:17 +0000

    Deploying to gh-pages from @ sardapv/sardapv.github.io@0ff1aac2d127e0a990f655df62fcfd2aeeeae55d 🚀

---
+++ _app/immutable/nodes/3.ec2b6214.js
@@ -0,0 +1,51 @@
+import{s as mt,c as pt,u as vt,g as wt,d as bt,n as _t}from"../chunks/scheduler.b732239f.js";import{S as ht,i as gt,g as l,h as i,j as C,f as w,k as g,a as T,z as $t,d as B,t as R,s as L,m as p,r as D,x as N,c as E,n as v,u as M,y as s,v as P,w as V}from"../chunks/index.664ab2c6.js";function yt(u){let t,e,o,c;const m=u[2].default,f=pt(m,u,u[1],null);return{c(){t=l("a"),f&&f.c(),this.h()},l(r){t=i(r,"A",{href:!0,class:!0});var n=C(t);f&&f.l(n),n.forEach(w),this.h()},h(){g(t,"href","#"),g(t,"class","svelte-9p78ys")},m(r,n){T(r,t,n),f&&f.m(t,null),e=!0,o||(c=$t(t,"click",u[3]),o=!0)},p(r,[n]){f&&f.p&&(!e||n&2)&&vt(f,m,r,r[1],e?bt(m,r[1],n,null):wt(r[1]),null)},i(r){e||(B(f,r),e=!0)},o(r){R(f,r),e=!1},d(r){r&&w(t),f&&f.d(r),o=!1,c()}}}function It(u,t,e){let{$$slots:o={},$$scope:c}=t,{src:m=""}=t;const f=()=>window.open(m,"_blank");return u.$$set=r=>{"src"in r&&e(0,m=r.src),"$$scope"in r&&e(1,c=r.$$scope)},[m,c,o,f]}class J extends ht{constructor(t){super(),gt(this,t,It,yt,mt,{src:0})}}function kt(u){let t;return{c(){t=p("COEP, Pune")},l(e){t=v(e,"COEP, Pune")},m(e,o){T(e,t,o)},d(e){e&&w(t)}}}function xt(u){let t;return{c(){t=p("Youtube")},l(e){t=v(e,"Youtube")},m(e,o){T(e,t,o)},d(e){e&&w(t)}}}function St(u){let t;return{c(){t=p("Hashnode")},l(e){t=v(e,"Hashnode")},m(e,o){T(e,t,o)},d(e){e&&w(t)}}}function Ct(u){let t,e='<span class="tracking-widest">...</span>This is the way!&quot;';return{c(){t=l("span"),t.innerHTML=e,this.h()},l(o){t=i(o,"SPAN",{class:!0,["data-svelte-h"]:!0}),N(t)!=="svelte-1qg0o50"&&(t.innerHTML=e),this.h()},h(){g(t,"class","font-semibold")},m(o,c){T(o,t,c)},p:_t,d(o){o&&w(t)}}}function Tt(u){let t,e,o,c,m,f="Hey 👋, I am Pranav. I live in Bengaluru, the Silicon Valley of India 🇮🇳",r,n,q,_,z,F,O,G,K,U,W,$,Y,y,Q,X,Z,tt,et,nt,st,at,ot,rt,lt,I,it,A,ct,k,ft='<h1 class="text-3xl font-semibold font-gilroy my-4">Tech I know 👨🏻‍💻</h1> <p class="text-slate-600">HTML, CSS/SCSS, JavaScript, Typescript , Angular, Sveltekit, StencilJs, Tailwind</p> <hr class="bg-zinc-300 border-0 h-px my-4"/> <h1 class="text-3xl font-semibold font-gilroy my-4">Tech I am learning 📚</h1> <p class="text-slate-600">React.js, Solid.js</p> <hr class="bg-zinc-300 border-0 h-px my-4"/> <h1 class="text-3xl font-semibold font-gilroy my-4">Non-Tech I am learning 🛸</h1> <p class="text-slate-600">3D animation and Tools, AI tools</p>',dt,x,ut='<img src="/images/me.jpg" alt="me" decoding="async" class="rounded-2xl rotate-3 object-cover shadow-sm bg-slate-800" height="500" width="350"/>',H;return _=new J({props:{src:"https://en.wikipedia.org/wiki/College_of_Engineering,_Pune",$$slots:{default:[kt]},$$scope:{ctx:u}}}),$=new J({props:{src:"https://www.youtube.com/@pranavcodesweb",$$slots:{default:[xt]},$$scope:{ctx:u}}}),y=new J({props:{src:"https://pranavsarda.hashnode.dev/",$$slots:{default:[St]},$$scope:{ctx:u}}}),I=new J({props:{src:"/images/meme.jpeg",$$slots:{default:[Ct]},$$scope:{ctx:u}}}),{c(){t=l("div"),e=l("div"),o=l("div"),c=l("div"),m=l("h1"),m.textContent=f,r=L(),n=l("p"),q=p("After graduating in 2018 from "),D(_.$$.fragment),z=p(`; I began my career at a product-oriented MNC. Fast forward to 2021, I shifted to
+					Bengaluru & I took a detour from full stack development and discovered my true calling in
+					frontend. Now, I'm on an epic adventure of crafting pixel-perfect UI designs and obsessing
+					over user experience.
+					`),F=l("br"),O=l("br"),G=p(`
+
+					I've worked with Angular, Svelte, StencilJs, and my favorite CSS framework, Tailwind. I
+					began my career in Java Spring backend and Ionic Cordova app development. At my current
+					gig, I am building intuitive dashboards and powerful fintech SDKs that let you integrate
+					banking flows with just a few lines of code. I believe in choosing the right tools for
+					each project to deliver the best results.
+
+					`),K=l("br"),U=l("br"),W=p(`
+					I'm also a newbie content creator on `),D($.$$.fragment),Y=p(`, diving into the world of animation. Check out my channel for exciting content.
+					Ocassionally I write blogs on `),D(y.$$.fragment),Q=p(` as well.
+
+					`),X=l("br"),Z=l("br"),tt=p(`
+					When I'm not busy slaying frontend dragons, I morph into a good husband who knows how to give
+					his wife and family the time and attention they deserve. Family is the ultimate superpower,
+					you know!
+					`),et=l("br"),nt=l("br"),st=p(`
+					And speaking of superpowers, I'm an unabashed sci-fi movie geek. I go gaga over anything that
+					involves time travel. Give me a good old-fashioned paradox, and I'm hooked!
+
+					`),at=l("br"),ot=l("br"),rt=p(`
+					Let's join forces, conquer the web, and spread smiles everywhere!.`),lt=l("br"),D(I.$$.fragment),it=L(),A=l("hr"),ct=L(),k=l("div"),k.innerHTML=ft,dt=L(),x=l("div"),x.innerHTML=ut,this.h()},l(d){t=i(d,"DIV",{class:!0});var h=C(t);e=i(h,"DIV",{class:!0});var j=C(e);o=i(j,"DIV",{class:!0});var S=C(o);c=i(S,"DIV",{class:!0});var b=C(c);m=i(b,"H1",{class:!0,["data-svelte-h"]:!0}),N(m)!=="svelte-2nkfmt"&&(m.textContent=f),r=E(b),n=i(b,"P",{class:!0});var a=C(n);q=v(a,"After graduating in 2018 from "),M(_.$$.fragment,a),z=v(a,`; I began my career at a product-oriented MNC. Fast forward to 2021, I shifted to
+					Bengaluru & I took a detour from full stack development and discovered my true calling in
+					frontend. Now, I'm on an epic adventure of crafting pixel-perfect UI designs and obsessing
+					over user experience.
+					`),F=i(a,"BR",{}),O=i(a,"BR",{}),G=v(a,`
+
+					I've worked with Angular, Svelte, StencilJs, and my favorite CSS framework, Tailwind. I
+					began my career in Java Spring backend and Ionic Cordova app development. At my current
+					gig, I am building intuitive dashboards and powerful fintech SDKs that let you integrate
+					banking flows with just a few lines of code. I believe in choosing the right tools for
+					each project to deliver the best results.
+
+					`),K=i(a,"BR",{}),U=i(a,"BR",{}),W=v(a,`
+					I'm also a newbie content creator on `),M($.$$.fragment,a),Y=v(a,`, diving into the world of animation. Check out my channel for exciting content.
+					Ocassionally I write blogs on `),M(y.$$.fragment,a),Q=v(a,` as well.
+
+					`),X=i(a,"BR",{}),Z=i(a,"BR",{}),tt=v(a,`
+					When I'm not busy slaying frontend dragons, I morph into a good husband who knows how to give
+					his wife and family the time and attention they deserve. Family is the ultimate superpower,
+					you know!
+					`),et=i(a,"BR",{}),nt=i(a,"BR",{}),st=v(a,`
+					And speaking of superpowers, I'm an unabashed sci-fi movie geek. I go gaga over anything that
+					involves time travel. Give me a good old-fashioned paradox, and I'm hooked!
+
+					`),at=i(a,"BR",{}),ot=i(a,"BR",{}),rt=v(a,`
+					Let's join forces, conquer the web, and spread smiles everywhere!.`),lt=i(a,"BR",{}),M(I.$$.fragment,a),a.forEach(w),it=E(b),A=i(b,"HR",{class:!0}),ct=E(b),k=i(b,"DIV",{class:!0,["data-svelte-h"]:!0}),N(k)!=="svelte-1tnmuos"&&(k.innerHTML=ft),b.forEach(w),dt=E(S),x=i(S,"DIV",{class:!0,["data-svelte-h"]:!0}),N(x)!=="svelte-2mlp0y"&&(x.innerHTML=ut),S.forEach(w),j.forEach(w),h.forEach(w),this.h()},h(){g(m,"class","text-4xl font-semibold font-gilroy"),g(n,"class","text-sm mt-5 font-normal text-slate-600"),g(A,"class","bg-zinc-300 border-0 h-px my-4"),g(k,"class","text-sm font-normal"),g(c,"class","lg:row-span-2"),g(x,"class","px-6 pt-4 lg:pt-20 flex justify-center order-first lg:order-last"),g(o,"class","grid lg:grid-cols-2 lg:grid-rows-[auto_1fr] gap-12"),g(e,"class","max-w-2xl lg:max-w-full"),g(t,"class","p-3 flex flex-col items-center h-full mx-[4vw] md:mx-[10vw] 2xl:mx-[20vw] justify-center")},m(d,h){T(d,t,h),s(t,e),s(e,o),s(o,c),s(c,m),s(c,r),s(c,n),s(n,q),P(_,n,null),s(n,z),s(n,F),s(n,O),s(n,G),s(n,K),s(n,U),s(n,W),P($,n,null),s(n,Y),P(y,n,null),s(n,Q),s(n,X),s(n,Z),s(n,tt),s(n,et),s(n,nt),s(n,st),s(n,at),s(n,ot),s(n,rt),s(n,lt),P(I,n,null),s(c,it),s(c,A),s(c,ct),s(c,k),s(o,dt),s(o,x),H=!0},p(d,[h]){const j={};h&1&&(j.$$scope={dirty:h,ctx:d}),_.$set(j);const S={};h&1&&(S.$$scope={dirty:h,ctx:d}),$.$set(S);const b={};h&1&&(b.$$scope={dirty:h,ctx:d}),y.$set(b);const a={};h&1&&(a.$$scope={dirty:h,ctx:d}),I.$set(a)},i(d){H||(B(_.$$.fragment,d),B($.$$.fragment,d),B(y.$$.fragment,d),B(I.$$.fragment,d),H=!0)},o(d){R(_.$$.fragment,d),R($.$$.fragment,d),R(y.$$.fragment,d),R(I.$$.fragment,d),H=!1},d(d){d&&w(t),V(_),V($),V(y),V(I)}}}class Rt extends ht{constructor(t){super(),gt(this,t,null,Tt,mt,{})}}export{Rt as component};
